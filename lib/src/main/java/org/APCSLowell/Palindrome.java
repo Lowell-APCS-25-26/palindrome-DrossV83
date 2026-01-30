a18eca7d9942c9505373b4fe55281ae204352f99
Jayden Vo <146142820+DrossV83@users.noreply.github.com>
2026-01-30 07:19:54 +0000

    fin

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,24 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+        String clean = nospace(s).toLowerCase(); 
+        return clean.equals(reverse(clean));
     }
     public String reverse(String s) {
-        String n = new String();
-        // your code here
-        return n;
+          String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed += s.charAt(i);
+        }
+        return reversed;
+    }
+    public String nospace(String s){
+        String spaceless = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetter(c)) {         
+                spaceless += c;
+            }
+        }
+        return spaceless;
     }
 }
